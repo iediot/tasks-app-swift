2262878f02ef4c339f1c2ee49cd51093fca23871
iediot <eduardnicolae092@gmail.com>
2026-03-01 22:19:42 +0200

    Initial Commit

---
+++ Tasks/TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasksApp.swift
+//  Tasks
+//
+//  Created by Tabirca Nicolae-Eduard on 01.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
